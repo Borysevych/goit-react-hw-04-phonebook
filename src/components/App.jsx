544868b873ae8298bf49fba4ba0bd369f6fe8692
Borysevych <borysevych.rus@gmail.com>
2023-05-21 21:40:53 +0300

    HW 4 react phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,80 @@
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter.jsx';
+import { ContactList } from './ContactList/ContactList';
+import { AppContainer } from './App.styled';
+import { useRef } from 'react';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const isFirstRender = useRef(true);
+
+  const filterContacts = e => setFilter(e.target.value.toLowerCase());
+
+  const onSubmitForm = ({ name, number }) => {
+    if (contacts.find(el => el.name === name)) {
+      alert(`${name} is alredy in contacts`);
+      return;
+    }
+    setContacts(state => [
+      ...state,
+      { id: nanoid(), name: name, number: number },
+    ]);
+  };
+
+  useEffect(() => {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+    console.log(parsedContacts);
+    if (parsedContacts) {
+      setContacts(parsedContacts);
+    }
+  }, []);
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+    contacts.length >= 0 &&
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const deleteContact = contactId => {
+    setContacts(state => state.filter(contact => contact.id !== contactId));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <AppContainer>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={onSubmitForm}></ContactForm>
+      {contacts.length > 0 && (
+        <div>
+          <h2>Contacts</h2>
+          <Filter filter={filter} onChangeValue={filterContacts} />
+
+          <ContactList
+            onDeleteContact={deleteContact}
+            contacts={contacts}
+            filter={filter}
+          />
+        </div>
+      )}
+    </AppContainer>
   );
+};
+
+//
+
+App.propTypes = {
+  onSubmit: PropTypes.func,
+  filter: PropTypes.string,
+  contacts: PropTypes.shape({
+    id: PropTypes.any.isRequired,
+    name: PropTypes.string.isRequired,
+    number: PropTypes.number.isRequired,
+    deleteContact: PropTypes.func,
+  }),
 };
